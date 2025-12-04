1ed47652db6c423b34af57399382ae757be28d0f
Paulo Henrique <paulohenrique1019@gmail.com>
2025-12-04 17:01:32 -0300

    investments aggregator

---
+++ src/main/java/com/paulodev/investmentsaggregator/InvestmentsAggregatorApplication.java
@@ -0,0 +1,11 @@
+package com.paulodev.investmentsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsAggregatorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsAggregatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/paulodev/investmentsaggregator/InvestmentsaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulodev.investmentsaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
